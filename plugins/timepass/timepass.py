6f37e635070361e02d21f91dd7f255421aac0624
Vish <vish@agilityroots.com>
2018-01-16 15:26:44 +0530

    #1 WIP - basic conditional flow based on NLP - understands whether command is tea  or coffee

--- plugins/timepass/timepass.py
+++ plugins/timepass/timepass.py
@@ -2,6 +2,8 @@ import random
 from errbot import botcmd, BotPlugin, botmatch
 class TimePass(BotPlugin):
 
+    # example flow copied from
+    # https://github.com/errbotio/err-guess-a-number
     @botcmd
     def tryme(self, msg, _):
         """Start to guess a number !"""
@@ -25,3 +27,24 @@ class TimePass(BotPlugin):
             return 'More ! %d tries left' % msg.ctx['tries']
 
         return 'Less! %d tries left' % msg.ctx['tries']
+
+    # trying to replicate same flow with NLP
+
+    @botcmd
+    def timepass(self,msg,_):
+        """Starts the Timepass Flow."""
+        self.log.debug(str(msg))
+        msg.ctx['should_end'] = False
+        return """Tell Me What To Do :+1:"""
+
+    @botmatch(r'^.*coffee.*$', flow_only=True)
+    def timepassing_coffee(self,msg,match):
+        msg.ctx['should_end'] = False
+        msg.ctx['orig_msg'] = str(msg)
+        return """Here is some :coffee: <br> your input: {0} match: {1}""".format(msg,match.string)
+
+    @botmatch(r'^.*tea.*$', flow_only=True)
+    def timepassing_tea(self,msg,match):
+        msg.ctx['should_end'] = False
+        msg.ctx['orig_msg'] = str(msg)
+        return """Here is some :tea: <br> your input: {0} match: {1}""".format(msg,match.string)
--- plugins/timepass/timepass_flows.py
+++ plugins/timepass/timepass_flows.py
@@ -1,7 +1,21 @@
 from errbot import botflow, FlowRoot, BotFlow, FLOW_END
-
+import spacy
 class TimepassFlows(BotFlow):
     """ Conversation flows related to polls"""
+
+    def is_coffee(orig_msg):
+        model = 'sm'
+        nlp = spacy.load('en_core_web_' + model)
+        coffee = nlp('i want coffee')
+        orig = nlp(orig_msg)
+        similarity = orig.similarity(coffee)
+        if (orig > 0.5):
+            return True
+        else:
+            return False
+
+    # example flow copied from
+    # https://github.com/errbotio/err-guess-a-number
 
     @botflow
     def guess(self, flow: FlowRoot):
@@ -11,3 +25,12 @@ class TimepassFlows(BotFlow):
         one_guess = game_created.connect('guessing')
         one_guess.connect(one_guess)  # loop on itself
         one_guess.connect(FLOW_END, predicate=lambda ctx: ctx['tries'] == 0)
+
+    # replicating this flow
+    @botflow
+    def timepass(self, flow: FlowRoot):
+        """This flow understands user's timepass needs and routes his requests."""
+        tp_created = flow.connect('timepass',auto_trigger=True)
+        tp_command = tp_created.connect('timepassing_coffee',predicate=lambda ctx: self.is_coffee(ctx['orig_msg']))
+        tp_command = tp_created.connect('timepassing_tea',predicate=lambda ctx: self.is_coffee(ctx['orig_msg']) == False)
+        tp_command.connect(FLOW_END,predicate=lambda ctx: ctx['should_end'] == True)
