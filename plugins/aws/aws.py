4f31f48a1dee6807b0a9fafdbefed9c9f6b64b54
Vish <vish@agilityroots.com>
2018-01-20 16:26:49 +0530

    #8 #9 #10 WIP - #8 cleared formatting, tested on slack and text interfaces. - #9 created some stub methods - #10 adding hourglass in slack, using yield in text interface.

--- plugins/aws/aws.py
+++ plugins/aws/aws.py
@@ -11,6 +11,16 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 class Aws(BotPlugin):
+
+    def _startProgress(self,msg):
+        if self._bot.mode == "slack":
+            self._bot.add_reaction(msg, "hourglass")
+
+    def _stopProgress(self,msg):
+        if self._bot.mode == "slack":
+            self._bot.remove_reaction(msg, "hourglass")
+
+
     def get_configuration_template(self):
         """ configuration entries """
         config = {
@@ -34,6 +44,32 @@ class Aws(BotPlugin):
         driver = cls(access_id, secret_key, region=region)
         return driver
 
+    def _list_amis(self,msg):
+        """
+        Private method, lists AWS AMIs.
+        """
+        self._startProgress(msg)
+        driver = self._connect()
+        account_id = self.config['account_id']
+        images = driver.list_images(ex_owner=account_id)
+        for img in images:
+            self.log.debug(img.id + img.name)
+        self._stopProgress(msg)
+        return images
+
+    def _get_ami_by_id(self,img):
+        return "TODO"
+
+    def _get_size(self,size):
+        return "TODO"
+
+    def _create_node(self,name,img,size):
+        """
+        Private method, creates instance from AMI.
+        """
+        driver = self._connect()
+        node = driver.create_node(name='test', image=img, size=size)
+        self.log.debug(node)
 
     @botcmd(split_args_with=' ')
     def aws_list_amis(self, msg, args):
@@ -41,16 +77,14 @@ class Aws(BotPlugin):
         list all amis belonging to owner.
         '''
         self.log.debug("""
-        >>>>>>
         from: {0}
         to: {1}
         is_direct: {2}
         is_group: {3}""".format(msg.frm,msg.to,msg.is_direct,msg.is_group))
-        ident = self.build_identifier(txtrep='#random/@vish')
-        self.log.debug(ident)
-        retVal = 'Images: '
-        for img in self._list_amis():
-            retVal += """id: `{0}` name: `{1}`""".format(img.id,img.name)
 
-        self.send(text=retVal,identifier=ident)
-    
+        retVal = 'Images in region {0}: \n'.format(self.config['region'])
+        imgs = self._list_amis(msg)
+        for img in imgs:
+            retVal += """id: `{0}`, name: `{1}`\n""".format(img.id,img.name)
+
+        self.send(msg.frm,retVal)
